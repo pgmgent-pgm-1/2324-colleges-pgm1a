c8c6164039ff48ea17ce27fdabe22f1cd29a4c85
Michael Vanderpoorten <michael@yungpanda.com>
2023-09-18 11:09:41 +0200

    Les 1

---
+++ week1/basis/start.js
@@ -0,0 +1,25 @@
+// met dit gaan we naar de console loggen
+console.log("Hallo 1PGMA hallo");
+
+// variablen aanmaken
+const firstName = "Michael";
+console.log(firstName);
+const age = 31;
+
+// old-school
+var lastName = "Vanderpoorten";
+
+// new-school: const & let
+// age = 32;  -> MAG NIET! want age is een const(ante)
+let c = 1;
+c = 2 + 2;
+console.log(c);
+
+// data types
+const name = "Michael Vanderpoorten"; // string
+const birthYear = 1992; // number
+const isLector = true; // boolean
+const pi = 3.14; // number
+
+console.log(name);
+console.log("Mijn naam is " + firstName + " " + lastName);
