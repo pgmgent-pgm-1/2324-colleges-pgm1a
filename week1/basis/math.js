1cb9ddd0d8b924d81832a40318ed2dc8974f269e
Michael Vanderpoorten <michael@yungpanda.com>
2023-09-19 12:06:53 +0200

    Math

---
+++ week1/basis/math.js
@@ -0,0 +1,4 @@
+// restwaarde
+console.log(10 % 3); // restwaarde 1
+console.log(10 % 5); // restwaarde 0
+console.log(4 % 3); // restwaarde 1;
---
+++ week1/oefeningen/math.js
@@ -0,0 +1,8 @@
+const x = 5;
+const y = 3;
+const z = 2;
+
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(`${x} - ${y} = ${x - y}`);
+console.log(`${x} * ${z} = ${x * z}`);
+console.log(`${x} / ${z} = ${x / z}`);
