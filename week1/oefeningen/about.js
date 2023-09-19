a18c4cd1250ecb3c1c4d87336cb36fd14b176961
Michael Vanderpoorten <michael@yungpanda.com>
2023-09-19 11:51:26 +0200

    Oefeningen

---
+++ week1/oefeningen/about.js
@@ -0,0 +1,8 @@
+const course = "Programmeren 1";
+const teacherOne = "Michael";
+const teacherTwo = "Philippe";
+
+// optie 1
+console.log("Ik krijg het vak " + course + " van " + teacherOne);
+// optie 2
+console.log(`Ik krijg het vak ${course} van ${teacherOne}`);
---
+++ week1/oefeningen/talk_like_a_pirate.js
@@ -0,0 +1,9 @@
+const pirate1Greeting = "Aye Aye!";
+const pirate1Speak = "Feed him to the fishes";
+const pirate2Greeting = "Ahoy!";
+const pirate2Speak = "Take what you can, give nothing back.";
+
+let talkLikeAPirate = "Pirate 1: " + pirate1Greeting + " " + pirate1Speak;
+console.log(talkLikeAPirate);
+talkLikeAPirate = "Pirate 2: " + pirate2Greeting + " " + pirate2Speak;
+console.log(talkLikeAPirate);
